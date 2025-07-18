752c53ac3808ec1109f7db70b92115397a9ac6e8
rafika khatun <rafikakhatun607@gmail.com>
2025-07-19 00:00:30 +0530

    Added footer section with services, company, and legal links using DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './component/Navbar'
 import LoginForm from './component/LoginForm'
+import Footer from './component/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
     <Navbar></Navbar>
     <LoginForm></LoginForm>
+    <Footer></Footer>
       
     </>
   )
